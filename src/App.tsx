47e8dc3046b47f494368937b17da39a0a2ecbe90
Ivan Valdez <ivnvaldz@gmail.com>
2026-07-05 23:29:00 -0300

    chore: scaffold Vite + React + TS project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-svh w-svw flex items-center justify-center bg-stone-900 text-white">
+      <h1 className="text-3xl font-bold">mapia</h1>
+    </div>
+  )
+}
+
+export default App
